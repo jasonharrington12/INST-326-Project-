cea615c84adaaacc02460c0627e610e45bf72a35
Jason Harrington <jharrin4@terpmail.umd.edu>
2025-12-14 19:28:09 -0500

    Create io_utils.py

---
+++ Project 4/io_utils.py
@@ -0,0 +1,21 @@
+import csv
+import json
+from pathlib import Path
+from library import Book
+
+def import_books_csv(catalog, path: Path):
+    with path.open(newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            book = Book(
+                row["title"],
+                row["isbn"],
+                int(row["year"]),
+                row["author"],
+                row["genre"]
+            )
+            catalog.add_item(book)
+
+def export_report(loan_manager, path: Path):
+    with path.open("w", encoding="utf-8") as f:
+        json.dump(loan_manager.checkouts, f, indent=2)
